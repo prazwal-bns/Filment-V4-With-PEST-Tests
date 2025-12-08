13e75f847279540fe0cfc99096990007480390bb
prazwal-bns <prajwalbns15@gmail.com>
2025-12-08 11:57:16 +0545

    Actionable Columns Plugin implementation

--- app/Filament/Resources/Courses/Tables/CoursesTable.php
+++ app/Filament/Resources/Courses/Tables/CoursesTable.php
@@ -2,12 +2,15 @@
 
 namespace App\Filament\Resources\Courses\Tables;
 
+use App\Filament\Resources\Departments\DepartmentResource;
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Shreejan\FilamentActionableColumns\Tables\Columns\ActionableColumn;
 
 class CoursesTable
 {
@@ -21,9 +24,23 @@ class CoursesTable
                     ->numeric()
                     ->badge()
                     ->sortable(),
-                TextColumn::make('department.name')
+                // TextColumn::make('department.name')
+                //     ->badge()
+                //     ->color('secondary')
+                //     ->sortable(),
+
+                ActionableColumn::make('department.name')
+                    ->label('Department')
                     ->badge()
                     ->color('secondary')
+                    ->tapAction(Action::make('viewDepartment')
+                        ->label('View Department')
+                        ->icon('heroicon-o-eye')
+                        ->action(function ( $record, array $data, $livewire): void {
+                            // Redirect to the department resource page
+                            redirect(DepartmentResource::getUrl('view', ['record' => $record]));
+                        })
+                    )
                     ->sortable(),
                 TextColumn::make('created_at')
                     ->dateTime()
